aa6cfe95fd4d64efc88a83f60b4127cea2261239
sDaCoder <supravadutta_sDa@outlook.com>
2024-01-02 22:44:49 +0530

    fifth commit

--- dist/script.js
+++ dist/script.js
@@ -20,16 +20,17 @@ radios.forEach(radio => {
     })
 })
 
-function deselection(options, deselect) {
-    deselect.addEventListener('click', function () {
-        options.forEach(option => {
-            option.previousElementSibling.checked = false
-            colourOpt(option)
-        });
-    })
+function deselection(options) {
+    options.forEach(option => {
+        option.previousElementSibling.checked = false
+        colourOpt(option)
+    });
 }
 
-deselection(options, document.getElementById("deselect"))
+// deselection(options, document.getElementById("deselect"))
+document.getElementById("deselect").addEventListener('click', function () {
+    deselection(options)
+})
 
 // ? LOGIC PART
 const QuesAns = {
@@ -42,38 +43,42 @@ const QuesAns = {
     "What is the name of the highest mountain peak of the world": ["Mount Everest", "Godwin Austin(K2)", "Kanchenjunga", "Mount Killimanjaro",],
 };
 
-function quesPrint(qNo)
-{
-    document.getElementById("qarea").textContent = `${qArr[qNo]}?`    
+function quesPrint(qNo) {
+    document.getElementById("qarea").textContent = `${qArr[qNo]}?`
 }
 
-function optPrint(qNo, options)
-{
+function optPrint(qNo, options) {
     options.forEach((option, i) => {
-        let optNum = String.fromCharCode(65 + i)
-        option.textContent = `(${optNum})  ${optArr[qNo][i]}`
+        // let optNum = String.fromCharCode(65 + i)
+        option.textContent = `  ${optArr[qNo][i]}`
     })
 }
 
-function controlWid(qNo, bars)
-{
-    let width = (qNo+1)/(qArr.length)*100
-    bars.forEach(bar =>{bar.style.width = `${width}%`})
+function controlWid(qNo, bars) {
+    let width = (qNo + 1) / (qArr.length) * 100
+    bars.forEach(bar => { bar.style.width = `${width}%` })
 }
 
 function labelPrint(qNo, qLabels) {
     qLabels.forEach(qLabel => {
-        qLabel.textContent = `${qNo+1}/${qArr.length}`
+        qLabel.textContent = `${qNo + 1}/${qArr.length}`
     });
 }
 
 function checkedRadio(radios) {
-    for(let i = 0; i < radios.length; i++)
-    {
+    for (let i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
             return radios[i]
         }
     }
+}
+
+function UIload(qNo) {
+    quesPrint(qNo)
+    optPrint(qNo, options)
+    controlWid(qNo, bars)
+    labelPrint(qNo, qLabels)
+    deselection(options)
 }
 
 const qArr = (Object.keys(QuesAns))
@@ -81,21 +86,46 @@ const optArr = (Object.values(QuesAns))
 const qLabels = document.querySelectorAll(".qLabel")
 
 let qNo = 0
+let score = 0
 let bars = document.querySelectorAll(".bar")
 quesPrint(qNo)
 optPrint(qNo, options)
 controlWid(qNo, bars)
 labelPrint(qNo, qLabels)
-deselection(options, document.getElementById("continue"))
 
 document.getElementById("continue").addEventListener('click', () => {
 
-    qNo++
-    quesPrint(qNo)
-    optPrint(qNo, options)
-    controlWid(qNo, bars)
-    labelPrint(qNo, qLabels)
-    deselection(options, document.getElementById("continue"))
-    
+    // console.log(checkedAns)
+    if (checkedRadio(radios) != null) {
+
+        const checkedAns = checkedRadio(radios).nextElementSibling.textContent
+
+        if (checkedAns == `  ${optArr[qNo][0]}`) {
+            score++
+        }
+        console.log(score)
+        qNo++
+        if (qNo < qArr.length) {
+            UIload(qNo)
+        }
+        else {
+            let resultStr = `You scored ${score * 100} out of ${qArr.length * 100}`
+            alert(resultStr)
+            location.reload()
+        }
+    }
+    else {
+        console.log(score)
+        qNo++
+        if (qNo < qArr.length) {
+            UIload(qNo)
+        }
+        else {
+            let resultStr = `You scored ${score * 100} out of ${qArr.length * 100}`
+            alert(resultStr)
+            location.reload()
+        }
+    }
+
 })
 
